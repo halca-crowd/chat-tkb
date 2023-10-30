213ae6258fd0bc6a595a353d2530f73137b203ff
notch_man <miyake.kentaro0815@mokkuriko.com>
2023-10-31 02:35:11 +0900

    環境変数の設定を追加

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+/// <reference types="vite/client" />
+interface ImportMetaEnv {
+    readonly VITE_API_URL: string
+    readonly VITE_WS_URL: string
+    // readonly VITE_MODE: string
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import * as path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
@@ -12,4 +13,5 @@ export default defineConfig({
         }),
         tsconfigPaths(),
     ],
+    envDir: path.resolve(__dirname, 'config/env'),
 })
