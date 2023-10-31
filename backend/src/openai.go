0146e0bb3e7357808804e6d9a4ca6147875a0c16
Tomoya Kanda <tomoya.kanda.2022b@mlab.info>
2023-10-31 19:24:04 +0100

    add function for chatting with context

--- backend/src/openai.go
+++ backend/src/openai.go
@@ -18,7 +18,7 @@ func createClient(http_client *http.Client, token string, orgID string) *openai.
 	}
 	config.HTTPClient = http_client
 	client := openai.NewClientWithConfig(config)
-	
+
 	return client
 }
 
@@ -112,6 +112,54 @@ func throwChatStreamRequest(client *openai.Client, model string, prompt string, 
 		fmt.Println(resp_i)
 		response += resp_i.Choices[0].Delta.Content
 	}
+	return response, nil
+}
+
+// 対話形式のモデルにEOFが出力されるまでリクエストを投げる。複数回の対話に対応
+func throwChatStreamRequests(client *openai.Client, model string, prompt string, max_tokens int, history []openai.ChatCompletionMessage) ([]openai.ChatCompletionMessage, error) {
+	messages := []openai.ChatCompletionMessage{
+		{
+			Role:    openai.ChatMessageRoleUser,
+			Content: prompt,
+		},
+	}
+	// if the history exists, insert history at first index of messages
+	if len(history) > 0 {
+		messages = append(messages, history...)
+	}
+	stream, err := client.CreateChatCompletionStream(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model:     model,
+			Messages:  messages,
+			Stream:    true,
+			MaxTokens: max_tokens,
+		},
+	)
+	if err != nil {
+		return messages, err
+	}
+
+	defer stream.Close()
+	response_text := ""
+	for {
+		resp_i, err := stream.Recv()
+		if err != nil {
+			if !errors.Is(err, io.EOF) {
+				return messages, err
+			}
+			break
+		}
+		fmt.Println(resp_i)
+		response_text += resp_i.Choices[0].Delta.Content
+	}
+	response := append(
+		messages,
+		openai.ChatCompletionMessage{
+			Role:    openai.ChatMessageRoleAssistant,
+			Content: response_text,
+		})
+
 	return response, nil
 }
 
